fb8671e4ab60b10d4c196bd53f746fc687d03148
Gloria Yang <70741942+ZhixinY99@users.noreply.github.com>
2020-10-07 22:06:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+class ShyOnionRabbit {
+    constructor(){
+        this.name = 'Wendy';
+        this.age = '18';
+        this.dream = 'Meeting her dream boy';
+        this.friends = [Wille, Winston, Cher];
+        this.seeking = 'Heart';
+        this.food = 'Carrots';
+        this.weapon = 'Carrots';
+        this.enemy = 'Open Onion Rabbit';
+    }
+
+    laugh(){
+        if(gainHeart) {
+            say (hahahahah);
+        } else {
+            say (Wuwuwuwuw);
+        }
+        //feel sad when not gain Heart --> face turns blue
+    }
+
+    cry(){
+        //cry when attacked
+    }
+
+    attack(enemy){
+        if (enemy)
+        carrot = weapon; 
+    }
+
+    jump(){
+
+    }
+
+    seekHeart(){
+        //she needs to seek enough heart to find her dream boy
+    }
+}
